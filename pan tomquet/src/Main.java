e71f423eee7f2de503754f3bf483e0b864318ce5
beator19 <beatrizmaribel.torres@gracia.lasalle.cat>
2024-10-11 13:08:35 +0200

    subir codigo

---
+++ pan tomquet/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String primerapalbra  = scanner.nextLine();
+        String segundapalbra = scanner.nextLine();
+        String  resultado = primerapalbra + segundapalbra;
+                ;
+
+        System.out.println(primerapalbra+ "amb " + segundapalbra);
+
+
+
+
+        scanner.close();
+
+
+    }
+}
